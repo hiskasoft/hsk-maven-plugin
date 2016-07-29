ca3a9112d4732e9f7bb880bc7192cec8e8900da0
Willyams Yujra <yracnet@gmail.com>
2016-07-29 10:33:31 -0400

    example case

--- quality-code-example/src/main/java/com/github/yracnet/example/ClientServ.java
+++ quality-code-example/src/main/java/com/github/yracnet/example/ClientServ.java
@@ -2,24 +2,17 @@ package com.github.yracnet.example;
 import com.github.yracnet.example.data.Client;
 import java.util.List;
 
-public interface ClientServ {
-	public List<Client> 
+public interface ClientServ {	public List<Client> 
 								filterClient
 								(Client filter) 
-																throws Exception;
-
-	public Client 
+																throws Exception;	public Client 
 								createClient
 								(Client
 																value) 
-																throws Exception;
-
-	public Client 
+																throws Exception;	public Client 
 								updateClient
 								(Client 
-																value) throws Exception;
-
-	public Client removeClient
+																value) throws Exception;	public Client removeClient
 								(Client
 																value) throws Exception;
 }
