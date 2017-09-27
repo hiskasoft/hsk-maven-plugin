88e5ee496c6e5a483d7c7e554077f5b084a5008a
Willyams Yujra <yracnet@gmail.com>
2017-09-27 14:19:53 -0400

    +configuracion sonartype

--- qualitycode-example/src/main/java/com/github/yracnet/example/AccountServ.java
+++ qualitycode-example/src/main/java/com/github/yracnet/example/AccountServ.java
@@ -1,29 +1,6 @@
-/**
- * Copyright © ${licence.year} ${licence.owner} (${licence.email})
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.github.yracnet.example;
 
 import com.github.yracnet.example.data.Account;
 import java.util.List;
 
-public interface AccountServ {
-	List<Account> filterAccount(Account filter) throws Exception;
-
-	Account createAccount(Account value) throws Exception;
-
-	Account updateAccount(Account value) throws Exception;
-
-	Account removeAccount(Account value) throws Exception;
-}
+public interface AccountServ {	List<Account> filterAccount(Account filter) throws Exception;	Account createAccount(Account value) throws Exception;	Account updateAccount(Account value) throws Exception;	Account removeAccount(Account value) throws Exception;}
